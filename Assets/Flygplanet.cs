6cedc3b19ac97b8bc7b770f0b9a25d820af90113
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2018-10-04 19:01:48 +0200

    Update 1
    Han rör sig

---
+++ Assets/Flygplanet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flygplanet : MonoBehaviour {
+
+    public SpriteRenderer Rend;
+    public float MoveSpeed;
+    public float RotationSpeed;
+    public Color Color;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Rend.Color = Color;
+        Movement();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+
+    }
+    void Movement()
+    {
+        transform.Translate(MoveSpeed * Time.deltaTime, 0, 0, Space.Self);
+        if (Input.GetKey(KeyCode.A))
+            {
+            transform.Rotate(0, 0, RotationSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -RotationSpeed * Time.deltaTime, Space.World);
+            Rend.color = new Color(1, 0, 0);
+        }
+
+    }
+}
+
